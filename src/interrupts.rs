905b2b6ef0cfc5991dd32c1910df0b3d90399778
7rah <zrah@protonmail.com>
2022-01-31 21:35:29 +0800

    page 08

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -2,7 +2,7 @@ use crate::{gdt, print, println, vga_buffer::delete};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -35,10 +35,25 @@ lazy_static! {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // new
         }
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+
         idt
     };
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: PageFaultErrorCode,
+) {
+    use crate::hit_loop;
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("{:#?}", stack_frame);
+    hit_loop();
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,39 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::{hit_loop, print, println};
+use blog_os::{hit_loop, println, serial_println};
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("[Kernel] Hello world{}", "!");
+    serial_println!("[Kernel] Hello world{}", "!");
     blog_os::init();
-    print!("[Kernel] Hello world again{}", "!");
-    //x86_64::instructions::interrupts::int3();
 
-    //fn stack() {
-    //    stack();
-    //}
-    //
-    //stack();
+    use x86_64::structures::paging::PageTable;
+
+    let level_4_table_ptr = 0x1000 as *const PageTable;
+    let level_4_table = unsafe { &*level_4_table_ptr };
+    for i in 0..512 {
+        if !level_4_table[i].is_unused() {
+            serial_println!("Entry {}: {:?}", i, level_4_table[i]);
+        }
+    }
+
+    //use x86_64::registers::control::Cr3;
+    //let (level_4_pagetable, _) = Cr3::read();
+    //println!(
+    //    "level 4 page table at: {:?}",
+    //    level_4_pagetable.start_address()
+    //);
+
+    //let ptr = 0x203fb1 as *mut u32;
+    //unsafe { let x = *ptr;println!("{x}"); }
+    //unsafe {*ptr = 42; }
+    println!("It did not crash");
 
     #[cfg(test)]
     test_main();
-    //blog_os::vga_buffer::print_something();
 
     hit_loop()
 }
